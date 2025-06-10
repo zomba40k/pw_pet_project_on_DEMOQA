bb092dd3fa88677fd4ec53d06e1e0730fb9c2cc4
zomba40k <astapov-n@bk.ru>
2025-06-10 10:35:38 +0300

    Added browser_windows_page tests

---
+++ pages/browser_windows_page.py
@@ -0,0 +1,34 @@
+from pages.base_page import BasePage
+from playwright.sync_api import Page, expect
+
+class BrowserWindowsPage(BasePage):
+    def __init__(self, page:Page):
+        super().__init__(page)
+        self.tab_btn = self.page.locator("#tabButton")
+        self.window_btn = self.page.locator("#windowButton")
+        self.message_window_btn = self.page.locator("#messageWindowButton")
+
+
+    def click_tab_btn(self):
+        with self.page.expect_popup() as popup_info:
+            self.tab_btn.click()
+
+        new_tab = popup_info.value
+        assert new_tab.url == 'https://demoqa.com/sample'
+        expect(new_tab.locator('body')).to_contain_text('This is a sample page')
+
+    def click_window_btn(self):
+        with self.page.expect_popup() as popup_info:
+            self.window_btn.click()
+        new_window = popup_info.value
+
+        expect(new_window.locator('body')).to_contain_text('This is a sample page')
+        assert new_window.url == 'https://demoqa.com/sample'
+
+    def click_window_msg_btn(self):
+        with self.page.expect_popup() as popup_info:
+            self.message_window_btn.click()
+
+        new_msg_window = popup_info.value
+        expect(new_msg_window.locator('body')).to_contain_text('Knowledge increases by sharing but not by saving. ')
+
---
+++ tests/test_browser_windows_page.py
@@ -0,0 +1,19 @@
+from pages.browser_windows_page import BrowserWindowsPage
+
+link = 'https://demoqa.com/browser-windows'
+class TestBrowserWindowsPage:
+
+    def test_new_tab(self,page):
+        tab = BrowserWindowsPage(page)
+        tab.open(link)
+        tab.click_tab_btn()
+
+    def test_new_window(self,page):
+        window = BrowserWindowsPage(page)
+        window.open(link)
+        window.click_window_btn()
+
+    def test_new_window_message(self,page):
+        message = BrowserWindowsPage(page)
+        message.open(link)
+        message.click_window_msg_btn()
