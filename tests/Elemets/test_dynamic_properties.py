57a83334cde8342b928319a0de6b012d55c2cba3
zomba40k <astapov-n@bk.ru>
2025-07-02 17:24:18 +0500

    changed last setup to be more polite

--- tests/Elemets/test_dynamic_properties.py
+++ tests/Elemets/test_dynamic_properties.py
@@ -9,19 +9,19 @@ link = 'https://demoqa.com/dynamic-properties'
 class TestDynamicProperties:
     @pytest.fixture(autouse=True)
     def setup(self,page):
-        self.yaica = DynamicPropertiesPage(page)
+        self.dynamic = DynamicPropertiesPage(page)
 
     @allure.title('Первая кнопка станет активной только через 5 секунда')
     def test_enable_after_btn(self):
-        self.yaica.open(link)
-        self.yaica.check_btn_enabled(self.yaica.enable_after_btn)
+        self.dynamic.open(link)
+        self.dynamic.check_btn_enabled(self.dynamic.enable_after_btn)
 
     @allure.title('Третья кнопка стнет видимой через 5 секунд')
     def test_visible_after_btn(self):
-        self.yaica.open(link)
-        self.yaica.check_btn_visible(self.yaica.visible_after_btn)
+        self.dynamic.open(link)
+        self.dynamic.check_btn_visible(self.dynamic.visible_after_btn)
 
     @allure.title('Вторая кнопка поменяет цвет через  5 секунд')
     def test_color_change_btn(self):
-        self.yaica.open(link)
-        self.yaica.check_btn_color(self.yaica.color_change_btn)
+        self.dynamic.open(link)
+        self.dynamic.check_btn_color(self.dynamic.color_change_btn)
